51c77b890b9115e53b8a111367bb21b7983ae6f9
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-11 22:19:49 +0100

    develop - fetch backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+	const [apiResponse, setApiResponse] = useState(null);
+
+	// Fetch API:
+	useEffect(() => {
+		const urlData = axios
+			.get("http://ec2-52-91-153-74.compute-1.amazonaws.com:8080/test/hola")
+			.then((res) => {
+				const response = res.data;
+				setApiResponse(response);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 	return (
 		<div className="App">
 			<h1>TestApp - Frontend!</h1>
+			<h2>Api Dice: {apiResponse}</h2>
 		</div>
 	);
 }
