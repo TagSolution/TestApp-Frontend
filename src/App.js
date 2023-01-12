b6b8f04f39a94fff7b9a030e82a891ae32d77117
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-12 16:38:14 +0100

    aws - posting image and retrieving

--- src/App.js
+++ src/App.js
@@ -3,23 +3,59 @@ import { useEffect, useState } from "react";
 
 function App() {
 	const [apiResponse, setApiResponse] = useState(null);
+	const [images, setImages] = useState([]);
 
-	// Fetch API:
-	useEffect(() => {
+	// API:
+	useEffect(() => {}, []);
+
+	// Functions:
+	function onImageChange(e) {
+		setImages([...e.target.files]);
+	}
+	function handleSubmit() {
+		const formData = new FormData();
+		formData.append("file", images[0]);
+		const config = {
+			headers: {
+				"content-type": "multipart/form-data",
+			},
+		};
 		axios
-			.get("https://testapp-tag.herokuapp.com/test/hola")
+			.post(
+				"https://testapp-tag.herokuapp.com/storage/upload",
+				formData,
+				config
+			)
 			.then((res) => {
-				const response = res.data;
-				setApiResponse(response);
+				console.log(res);
+				const imageUrl = res.data.imageUrl;
+				setApiResponse(imageUrl);
 			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, []);
+			.catch((e) => console.log(e));
+	}
+
 	return (
 		<div className="App">
-			<h1>TestApp - Frontend!</h1>
-			<h2>Api Dice: {apiResponse}</h2>
+			<h1 className="title">TestApp - Frontend!</h1>
+
+			<div className="aws-container">
+				<div className="aws-input">
+					<input
+						type="file"
+						name="inputImage"
+						id="inputImg"
+						onChange={onImageChange}
+					/>
+					<button type="submit" onClick={handleSubmit}>
+						Send
+					</button>
+				</div>
+				{apiResponse ? (
+					<img src={apiResponse} alt="SavedImage" />
+				) : (
+					"Suba una imagen!"
+				)}
+			</div>
 		</div>
 	);
 }
